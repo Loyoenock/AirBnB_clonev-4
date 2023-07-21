d571c4d5b5b84c21a2f816af7440ac58c244cde5
Loyoenock <loyoenock@gmail.com>
2023-07-20 23:10:15 -0700

    Initial Project files 0x04. AirBnB clone - Web framework

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script contains the `do_pack` function, which generates a compressed archive file (.tgz)
+from the contents of the `web_static` folder using the Fabric library.
+"""
+
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
